1c73a034ca0fa1dc42c18515951efd8e499813d6
Wendy-Tabitha <akinyiwendy46@gmail.com>
2025-07-28 11:37:21 +0300

    feat: Implement user registration functionality
    **Description:**
    Adds a new user registration feature that allows users to create accounts by entering a username and password. The system checks for existing usernames and securely saves new user credentials to the data file, enhancing the user management capabilities of the ATM system.

--- src/auth.c
+++ src/auth.c
@@ -10,7 +10,7 @@ char *USERS = "./data/users.txt";
 void loginMenu(char a[50], char pass[50])
 {
     system("clear");
-    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t Username:");
     scanf("%s", a);
 
     printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
@@ -51,6 +51,85 @@ void loginMenu(char a[50], char pass[50])
         exit(1);
     }
 #endif
+}
+
+void registerMenu(char a[50], char pass[50])
+{
+    FILE *fp;
+    struct User userChecker;
+    int maxId = -1;
+    char tempName[50];
+    char tempPass[50];
+
+    system("clear");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Registration:\n");
+    printf("Enter your username: ");
+    scanf("%s", tempName);
+
+    // Check if username already exists
+    if ((fp = fopen("./data/users.txt", "r")) != NULL)
+    {
+        while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
+        {
+            if (strcmp(userChecker.name, tempName) == 0)
+            {
+                printf("\nUsername already exists!\n");
+                fclose(fp);
+                strcpy(a, "");
+                strcpy(pass, "");
+                return;
+            }
+            if (userChecker.id > maxId) maxId = userChecker.id;
+        }
+        fclose(fp);
+    }
+
+    printf("Enter your password: ");
+#if defined(_WIN32) || defined(_WIN64)
+    int i = 0;
+    char ch;
+    while ((ch = _getch()) != '\r' && i < 49)
+    {
+        if (ch == '\b' && i > 0)
+        {
+            i--;
+            printf("\b \b");
+        }
+        else if (ch != '\b')
+        {
+            tempPass[i++] = ch;
+            printf("*");
+        }
+    }
+    tempPass[i] = '\0';
+    printf("\n");
+#else
+    struct termios oflags, nflags;
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
+    {
+        perror("tcsetattr");
+        exit(1);
+    }
+    scanf("%s", tempPass);
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    {
+        perror("tcsetattr");
+        exit(1);
+    }
+#endif
+
+    // Save new user
+    fp = fopen("./data/users.txt", "a");
+    fprintf(fp, "%d %s %s\n", maxId + 1, tempName, tempPass);
+    fclose(fp);
+
+    strcpy(a, tempName);
+    strcpy(pass, tempPass);
+    printf("\nRegistration successful!\n");
 }
 
 const char *getPassword(struct User u)
